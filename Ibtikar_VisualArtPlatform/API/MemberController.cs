c1f2f7fa24ca6c719088a33df3eff895bf4233e2
Ahmed El-Kadi <ahmed.el-kadi@itworxedu.com>
2020-12-27 13:10:47 +0200

    adding bulkmember activities

---
+++ Ibtikar_VisualArtPlatform/API/MemberController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Services;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.WebApi;
+
+namespace Ibtikar_VisualArtPlatform.API
+{
+    [PluginController("BulkMemberActivities")]
+    public class MemberController : UmbracoApiController
+    {
+        private readonly IMemberService memberService;
+
+        public MemberController(IMemberService memberService)
+        {
+            this.memberService = memberService;
+        }
+        public IEnumerable<string> GetAllMembers()
+        {
+
+            var members = memberService.GetAllMembers();
+
+            return members.Select(x => x.Name + "-" + x.IsApproved.ToString());
+        }
+    }
+}
--- Ibtikar_VisualArtPlatform/App_Plugins/bulkMemberActivities/backoffice/bulkMemberActivities/bulkMemberActivities.controller.js
+++ Ibtikar_VisualArtPlatform/App_Plugins/bulkMemberActivities/backoffice/bulkMemberActivities/bulkMemberActivities.controller.js
@@ -1,9 +1,14 @@
-﻿angular.module("umbraco").controller("Umbraco.Members.BulkMemberActivities", function ($scope) {
+﻿angular.module("umbraco").controller("Umbraco.Members.BulkMemberActivities", function ($scope, bulkMemberActivitiesService) {
     vm = this;
     vm.testnew = "TEst";
     console.log("enterned");
-    vm.content = { name : "Test 2" };
+    vm.content = { name: "Test 2" };
+    bulkMemberActivitiesService.getMembers()
+        .then(function (data) {
 
+            vm.members = data;
+            console.log(vm.members);
+        });
     vm.items = [
         {
             "icon": "icon-document",
@@ -35,30 +40,66 @@
     vm.isSortDirection = isSortDirection;
     vm.sort = sort;
 
-    function selectAll($event) {
-        alert("select all");
+    //function selectAll($event) {
+    //    console.log("selectAll", $event);
+    //}
+
+    //function isSelectedAll() {
+    //    console.log("isSelectedAll");
+
+    //}
+
+    //function clickItem(item) {
+    //    console.log("clickItem", item);
+
+
+    //}
+
+    //function selectItem(selectedItem, $index, $event) {
+    //    console.log("selectItem", selectItem, $index, $event);
+
+    //}
+
+    //function isSortDirection(col, direction) {
+    //    console.log("isSortDirection", col, direction);
+
+    //}
+
+    //function sort(field, allow, isSystem) {
+    //    console.log("sort", field, allow, isSystem);
+
+    //}
+
+
+    function selectAll() {
+       // listViewHelper.selectAllItemsToggle(vm.items, $scope.selection);
+
     }
 
     function isSelectedAll() {
-
+        //    return listViewHelper.isSelectedAll($scope.items, $scope.selection);
     }
 
     function clickItem(item) {
-        alert("click node");
+        //listViewHelper.editItem(item);
     }
 
-    function selectItem(selectedItem, $index, $event) {
-        alert("select node");
+    function selectItem(item, $index, $event) {
+        ///listViewHelper.selectHandler(item, $index, vm.items, $scope.selection, $event);
+
+
     }
 
     function isSortDirection(col, direction) {
-
+        //return listViewHelper.setSortingDirection(col, direction, $scope.options);
     }
-
     function sort(field, allow, isSystem) {
-
+        //if (allow) {
+        //    $scope.options.orderBySystemField = isSystem;
+        //    listViewHelper.setSorting(field, allow, $scope.options);
+        //    //  $scope.getContent($scope.contentId);
+        //}
     }
-
 
 
 
