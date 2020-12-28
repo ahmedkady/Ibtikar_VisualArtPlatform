a059f4137add3ece6e08ab86b3d0fe755c26304f
Ahmed El-Kadi <ahmed.el-kadi@itworxedu.com>
2020-12-28 14:46:19 +0200

    bulk

--- Ibtikar_VisualArtPlatform/App_Plugins/bulkMemberActivities/backoffice/bulkMemberActivities/bulkMemberActivities.controller.js
+++ Ibtikar_VisualArtPlatform/App_Plugins/bulkMemberActivities/backoffice/bulkMemberActivities/bulkMemberActivities.controller.js
@@ -1,4 +1,4 @@
-﻿angular.module("umbraco").controller("Umbraco.Members.BulkMemberActivities", function ($scope, bulkMemberActivitiesService) {
+﻿angular.module("umbraco").controller("Umbraco.Members.BulkMemberActivities", function ($scope, bulkMemberActivitiesService, listViewHelper ) {
     vm = this;
     vm.testnew = "TEst";
     console.log("enterned");
@@ -12,26 +12,36 @@
     vm.items = [
         {
             "icon": "icon-document",
-            "name": "My node 1",
+            "name": "My node 2",
             "published": true,
             "description": "A short description of my node",
             "author": "Author 1"
         },
         {
             "icon": "icon-document",
-            "name": "My node 2",
+            "name": "My node 1",
             "published": true,
             "description": "A short description of my node",
             "author": "Author 2"
         }
     ];
+        
 
     vm.options = {
+        filter: '',
+        orderBy: "name",
+        orderDirection: "asc",
         includeProperties: [
-            { alias: "description", header: "Description" },
-            { alias: "author", header: "Author" }
-        ]
+            { alias: "author", header: "Author" },
+            { alias: "description", header: "description" },
+            { alias: "name", header: "name" },
+            { alias: "published", header: "published" }
+        ],
+        bulkActionsAllowed: true
     };
+
+
+    vm.selection = [];
 
     vm.selectItem = selectItem;
     vm.clickItem = clickItem;
@@ -39,68 +49,40 @@
     vm.isSelectedAll = isSelectedAll;
     vm.isSortDirection = isSortDirection;
     vm.sort = sort;
-
-    //function selectAll($event) {
-    //    console.log("selectAll", $event);
-    //}
-
-    //function isSelectedAll() {
-    //    console.log("isSelectedAll");
-
-    //}
-
-    //function clickItem(item) {
-    //    console.log("clickItem", item);
-
-
-    //}
-
-    //function selectItem(selectedItem, $index, $event) {
-    //    console.log("selectItem", selectItem, $index, $event);
-
-    //}
-
-    //function isSortDirection(col, direction) {
-    //    console.log("isSortDirection", col, direction);
-
-    //}
-
-    //function sort(field, allow, isSystem) {
-    //    console.log("sort", field, allow, isSystem);
-
-    //}
-
+    vm.allowSelectAll = true;
 
     function selectAll() {
-       // listViewHelper.selectAllItemsToggle(vm.items, $scope.selection);
-
+        console.log("selectAll()");
+        listViewHelper.selectAllItemsToggle(vm.items, vm.selection);
     }
 
     function isSelectedAll() {
-        //    return listViewHelper.isSelectedAll($scope.items, $scope.selection);
+        console.log("isSelectedAll()");
+        return listViewHelper.isSelectedAll(vm.items, vm.selection);
     }
 
     function clickItem(item) {
+        console.log(item);
         //listViewHelper.editItem(item);
     }
 
     function selectItem(item, $index, $event) {
-        ///listViewHelper.selectHandler(item, $index, vm.items, $scope.selection, $event);
-
-
+        console.log("selectItem", item, $index, $event);
+        listViewHelper.selectHandler(item, $index, vm.items, vm.selection, $event);
     }
 
     function isSortDirection(col, direction) {
-        //return listViewHelper.setSortingDirection(col, direction, $scope.options);
-    }
-    function sort(field, allow, isSystem) {
-        //if (allow) {
-        //    $scope.options.orderBySystemField = isSystem;
-        //    listViewHelper.setSorting(field, allow, $scope.options);
-        //    //  $scope.getContent($scope.contentId);
-        //}
+        console.log("isSortDirection", col, direction);
+        return listViewHelper.setSortingDirection(col, direction, vm.options);
     }
 
+    function sort(field, allow) {
+        console.log("sort", field, allow);
+        if (allow) {
+            listViewHelper.setSorting(field, allow, vm.options);
+            //  $scope.getContent($scope.contentId);
+        }
+    }
 
 
 });
